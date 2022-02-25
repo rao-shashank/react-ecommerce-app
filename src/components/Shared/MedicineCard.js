83afb25403e2e3fdf87742224e011b4c18773986
Shashank Rao <shashank.rao@maplelabs.com>
2022-02-25 23:29:08 +0530

    allow to add items into the cart from listing page

--- src/components/Shared/MedicineCard.js
+++ src/components/Shared/MedicineCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { makeStyles,
   Card,
   CardActionArea,
@@ -10,6 +10,7 @@ import { makeStyles,
   Grid } from '@material-ui/core/';
 
 import MedImage from '../../assets/medicine-item.jpg';
+import CartContext from '../../store/CartContext';
 
 const useStyles = makeStyles({
   root: {
@@ -22,17 +23,28 @@ const useStyles = makeStyles({
 });
 
 const MedicineCard = (props) => {
+  const cartCtx = useContext(CartContext);
   const classes = useStyles();
 
   const onClickHandler = () => {
     props.onClick(props);
   };
 
+const addToCartHandler = () => {
+  cartCtx.addItem({
+    id: props.id,
+    name: props.name,
+    qty: 1,
+    price: props.price
+});
+};
+
   return (
     <Grid item xs={12} sm={6} md={3}>
-      <Card className={classes.root} onClick={onClickHandler}>
+      <Card className={classes.root}>
         <CardActionArea>
           <CardMedia
+            onClick={onClickHandler}
             component="img"
             alt="Contemplative Reptile"
             height="140"
@@ -52,8 +64,8 @@ const MedicineCard = (props) => {
           <h4 size="small" color="primary">
             <span>&#8377;</span> {props.price.toFixed(2)}
           </h4>
-          <Button size="small" color="primary">
-            View Details
+          <Button size="small" color="primary" onClick={addToCartHandler}>
+            + Add To Cart
           </Button>
         </CardActions>
       </Card>
